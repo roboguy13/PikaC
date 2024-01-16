a743b3bccef94001e2adac8b1d16f371f0cf69aa
David Young <roboguy06@gmail.com>
2024-01-16 02:46:53 -0600

    a small fix

--- tests/haskell/Take.hs
+++ tests/haskell/Take.hs
@@ -4,9 +4,6 @@ myTake :: Nat -> [Int] -> [Int]
 myTake Z _ = []
 myTake (S n) (x:xs) = x : myTake n xs
 
-theList :: [Int]
-theList = [1..100000]
-
 main :: IO ()
 main = print $ myTake (fromInt 5000) theList
 
