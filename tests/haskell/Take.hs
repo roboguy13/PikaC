b0cee05154a15d8cf1066785eb4dd4ad403a75cc
David Young <roboguy06@gmail.com>
2024-01-16 02:52:20 -0600

    Fix Haskell 'Take' benchmark

--- tests/haskell/Take.hs
+++ tests/haskell/Take.hs
@@ -3,6 +3,7 @@ import Common
 myTake :: Nat -> [Int] -> [Int]
 myTake Z _ = []
 myTake (S n) (x:xs) = x : myTake n xs
+mkTake (S _) xs = xs
 
 main :: IO ()
 main = print $ myTake (fromInt 5000) theList
